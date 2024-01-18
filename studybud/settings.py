5fedae93611ae09fb58ccb4682574b1f0fcad399
mwigoti <henrymwoha02@gmail.com>
2024-01-18 23:16:26 +0300

    studypal

--- studybud/settings.py
+++ studybud/settings.py
@@ -26,6 +26,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
+ALLOWED_HOSTS = ['*']
+
 import os
 import dj_database_url
 
